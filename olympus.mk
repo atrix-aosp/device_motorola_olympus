b959638fd55110a7820025d64af7ae3ebe0ffe55
Bryan D. Stine <stinebd@xdandroid.com>
2011-08-18 13:51:24 -0400

    Add APNs list from CyanogenMod and rearrange device makefile so that inherited prebuilts are overridden.

--- olympus.mk
+++ olympus.mk
@@ -28,9 +28,6 @@ PRODUCT_COPY_FILES += \
     device/motorola/olympus/init.olympus.rc:root/init.olympus.rc \
     device/motorola/olympus/ueventd.olympus.rc:root/ueventd.olympus.rc
 
-## (2) Also get non-open-source CDMA-specific aspects if available
-$(call inherit-product-if-exists, vendor/motorola/olympus/olympus-vendor.mk)
-
 # motorola pds permission fix script
 PRODUCT_COPY_FILES += \
     device/motorola/olympus/pds_perm_fix.sh:system/bin/pds_perm_fix.sh \
@@ -59,10 +56,6 @@ endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
-
-$(call inherit-product-if-exists, vendor/motorola/olympus/olympus-vendor.mk)
-
-$(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_PACKAGES += Usb \
     Superuser \
@@ -112,6 +105,11 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+## (2) Also get non-open-source CDMA-specific aspects if available
+$(call inherit-product-if-exists, vendor/motorola/olympus/olympus-vendor.mk)
+
+$(call inherit-product, build/target/product/full.mk)
+
 PRODUCT_NAME := generic_olympus
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := olympus
@@ -119,3 +117,4 @@ PRODUCT_MODEL := MB860
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=olyatt BUILD_ID=4.5.91 BUILD_DISPLAY_ID=4.5.91 BUILD_FINGERPRINT=MOTO/olyatt/olympus:2.3.4/4.5.91/110625:user/release-keys PRIVATE_BUILD_DESC="olympus-user 2.3.4 4.5.91 110625 release-keys"
 PRODUCT_SPECIFIC_DEFINES = WEBCORE_ACCELERATED_SCROLLING=true
+
